4912a4c025e6a784a5a4236c4e1082cb40a3f292
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-03-09 09:07:09 +0530

    Refactored the Cell object

---
+++ src/models/Cell.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Cell {
+    private BoardElement boardElement;
+
+    public BoardElement getBoardElement() {
+        return boardElement;
+    }
+
+    public void setBoardElement(BoardElement boardElement) {
+        this.boardElement = boardElement;
+    }
+}
